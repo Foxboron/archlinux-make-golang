13ae90d265ad054b908f128238f2ee89c8b816e3
Morten Linderud <morten@linderud.pw>
2018-07-30 00:41:06 +0200

    Added templates and dependency building
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- make.go
+++ make.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"html/template"
 	"log"
 	"os"
+	"os/exec"
+	"path"
 	"strings"
 
 	dh "github.com/foxboron/dh-make-golang/src"
@@ -33,6 +35,20 @@ var ArchSpecialLincenses = map[string]string{
 	"mit":          "MIT",
 }
 
+type Pkgbuild struct {
+	Pkgname            string
+	Pkgver             string
+	Pkgrel             string
+	Description        string
+	Lisence            string
+	Url                string
+	NonStandardLisence bool
+	Depends            []string
+	Revision           string
+	Repository         string
+	DirectoryName      string
+}
+
 func GetLicense(gopkg string) (string, bool, error) {
 	githubLicense, err := dh.GetLicenseForGopkg(gopkg)
 	if err != nil {
@@ -47,6 +63,106 @@ func GetLicense(gopkg string) (string, bool, error) {
 		return license, true, nil
 	}
 	return "TODO", false, fmt.Errorf("Could not determine lincense")
+}
+
+func GetRevision(gitdir string) (string, error) {
+	cmd := exec.Command("git", "rev-parse", "HEAD")
+	cmd.Dir = gitdir
+	revision, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	rev := string(revision)
+	return strings.TrimSpace(rev), nil
+}
+
+func CreatePackage(directory, gopkg, gopath, gitRevision, pkgType string) []string {
+	// Ensure the specified argument is a Go package import path.
+	rr, err := vcs.RepoRootForImportPath(gopkg, false)
+	if err != nil {
+		log.Fatalf("Verifying arguments: %v — did you specify a Go package import path?", err)
+	}
+	if gopkg != rr.Root {
+		log.Printf("Continuing with repository root %q instead of specified import path %q", rr.Root, gopkg)
+		gopkg = rr.Root
+	}
+
+	if strings.ToLower(gopkg) != gopkg {
+		log.Printf("WARNING: Go package names are case-sensitive. Did you really mean %q instead of %q?\n",
+			gopkg, strings.ToLower(gopkg))
+	}
+
+	u, err := dh.NewUpstreamSource(gopath, gopkg, gitRevision)
+	if err != nil {
+		log.Printf("Could not create a tarball of the upstream source: %v\n", err)
+		return []string{}
+	}
+
+	if strings.TrimSpace(pkgType) == "" {
+		if u.FirstMain != "" {
+			log.Printf("Assuming you are packaging a program (because %q defines a main package), use -type to override\n", u.FirstMain)
+			pkgType = "program"
+		} else {
+			pkgType = "library"
+		}
+	}
+	var pkgname string
+	allowUnknownHoster := true
+	pkgname = dh.NameFromGopkg(gopkg, pkgType, allowUnknownHoster)
+
+	if _, err := os.Stat(path.Join(directory, pkgname)); err == nil {
+		log.Printf("Output directory %q already exists, quitting\n", path.Join(directory, pkgname))
+		return u.RepoDeps
+	}
+
+	dependencies := make([]string, 0, len(u.RepoDeps))
+	for _, dep := range u.RepoDeps {
+		dependencies = append(dependencies, dh.NameFromGopkg(dep, "library", allowUnknownHoster))
+	}
+
+	license, special, err := GetLicense(gopkg)
+	if err != nil {
+		license = "TODO"
+		log.Printf("%s missing lisence. Marked as TODO", pkgname)
+	}
+
+	desc, err := dh.GetDescriptionForGopkg(gopkg)
+	if err != nil {
+		desc = "TODO"
+		log.Printf("%s missing description. Marked as TODO", pkgname)
+	}
+
+	rev, err := GetRevision(u.RepoDir)
+	if err != nil {
+		rev = "TODO"
+		log.Printf("%s missing revision. Marked as TODO", pkgname)
+	}
+
+	pkgbuild := &Pkgbuild{
+		Pkgname:            pkgname,
+		Pkgver:             u.Version,
+		Description:        desc,
+		Lisence:            license,
+		Url:                dh.GetHomepageForGopkg(gopkg),
+		NonStandardLisence: special,
+		Depends:            dependencies,
+		Revision:           rev,
+		Repository:         gopkg,
+		DirectoryName:      path.Base(gopkg),
+	}
+	os.Mkdir(path.Join(directory, pkgname), 0755)
+	f, err := os.Create(path.Join(directory, pkgname, "PKGBUILD"))
+	if err != nil {
+		log.Printf("Couldn't create file: %s", err)
+	}
+	tpl := template.Must(template.New("main").Funcs(template.FuncMap{"StringsJoin": strings.Join}).ParseGlob("./templates/*.template"))
+	err = tpl.ExecuteTemplate(f, fmt.Sprintf("pkgbuild-%s.template", pkgType), pkgbuild)
+	if err != nil {
+		log.Printf("Couldn't write templtae: %s", err)
+		return u.RepoDeps
+	}
+	log.Printf("Created package %s in %s", pkgname, path.Join(directory, pkgname))
+	return u.RepoDeps
 }
 
 func execMake(args []string) {
@@ -66,6 +182,11 @@ func execMake(args []string) {
 		"",
 		"One of \"library\" or \"program\"")
 
+	packageDependencies := fs.Bool(
+		"deps",
+		false,
+		"Package dependencies of the library/program")
+
 	err := fs.Parse(args)
 	if err != nil {
 		log.Fatal(err)
@@ -78,68 +199,39 @@ func execMake(args []string) {
 
 	gitRevision = strings.TrimSpace(gitRevision)
 	gopkg := fs.Arg(0)
-
-	// Ensure the specified argument is a Go package import path.
-	rr, err := vcs.RepoRootForImportPath(gopkg, false)
-	if err != nil {
-		log.Fatalf("Verifying arguments: %v — did you specify a Go package import path?", err)
-	}
-	if gopkg != rr.Root {
-		log.Printf("Continuing with repository root %q instead of specified import path %q", rr.Root, gopkg)
-		gopkg = rr.Root
-	}
-
-	if strings.ToLower(gopkg) != gopkg {
-		log.Printf("WARNING: Go package names are case-sensitive. Did you really mean %q instead of %q?\n",
-			gopkg, strings.ToLower(gopkg))
-	}
-
-	gopath, err := ioutil.TempDir("", "dh-make-golang")
-	if err != nil {
-		log.Fatalf("Could not create a tmp directory")
-	}
+	os.Mkdir("./packages", 0755)
+	// gopath, err := ioutil.TempDir("/var/tmp", "dh-make-golang")
+	// if err != nil {
+	// 	log.Fatalf("Could not create a tmp directory")
+	// }
+	gopath := "/var/tmp/dh-make-golang"
+	os.Mkdir(gopath, 0755)
 	defer os.RemoveAll(gopath)
 
-	u, err := dh.NewUpstreamSource(gopath, gopkg, gitRevision)
-	if err != nil {
-		log.Fatalf("Could not create a tarball of the upstream source: %v\n", err)
+	dependencies := CreatePackage("./packages", gopkg, gopath, gitRevision, pkgType)
+	if !*packageDependencies {
+		log.Println("Dependencies:")
+		for _, k := range dependencies {
+			log.Printf("\t%s", k)
+		}
+		os.Exit(0)
 	}
 
-	if strings.TrimSpace(pkgType) == "" {
-		if u.FirstMain != "" {
-			log.Printf("Assuming you are packaging a program (because %q defines a main package), use -type to override\n", u.FirstMain)
-			pkgType = "program"
-		} else {
-			pkgType = "library"
+	var dep string
+	for {
+		dep, dependencies = dependencies[0], dependencies[1:]
+		newDeps := CreatePackage("./packages", dep, gopath, "", "library")
+		for _, k := range newDeps {
+			new := true
+			for _, k2 := range dependencies {
+				if k == k2 {
+					new = false
+					break
+				}
+			}
+			if new {
+				dependencies = append(dependencies, k)
+			}
 		}
 	}
-	var pkgname string
-	allowUnknownHoster := true
-	pkgname = dh.NameFromGopkg(gopkg, pkgType, allowUnknownHoster)
-
-	if _, err := os.Stat(pkgname); err == nil {
-		log.Fatalf("Output directory %q already exists, aborting\n", pkgname)
-	}
-
-	dependencies := make([]string, 0, len(u.RepoDeps))
-	for _, dep := range u.RepoDeps {
-		dependencies = append(dependencies, dh.NameFromGopkg(dep, "library", allowUnknownHoster))
-	}
-
-	license, special, err := GetLicense(gopkg)
-	if err != nil {
-		log.Println(err)
-	}
-
-	fmt.Println(gopath)
-	fmt.Println(gopkg)
-	fmt.Println(pkgname)
-	fmt.Println(u.Version)
-	fmt.Println(pkgType)
-	fmt.Println(dependencies)
-	fmt.Println(u.RepoDeps)
-	fmt.Println(u.VendorDirs)
-	fmt.Println(license)
-	fmt.Println(special)
-
 }
